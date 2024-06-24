20d68dd6b3bbbc6ecdbbf3de75213a9c361a2ce2
João Henrique <ribeiro95joao@gmail.com>
2024-06-24 19:30:32 -0300

    app finalizado

---
+++ IsAPrimeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Is a Prime Number??");
+        Console.WriteLine("Enter an integer number:");
+
+        var numberTyped = Convert.ToInt32(Console.ReadLine());
+        int aux = 0;
+
+        for (int i = 1; i <= numberTyped; i++)
+        {
+            if (numberTyped % i == 0)
+            {
+                aux++;
+            }
+        }
+
+        if (aux <= 2)
+        {
+            Console.WriteLine("It's a Prime Number!!");
+        }
+        else
+        {
+            Console.WriteLine("Isn't a Prime Number.");
+        }
+    }
+}
+
